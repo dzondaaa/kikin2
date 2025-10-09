205cce5ddd25d8c891b36710cdb4958080f77011
dzondaaa <j.slepicka@icloud.com>
2025-10-09 13:13:47 +0200

    test

--- negri_do_plynu/Form1.cs
+++ negri_do_plynu/Form1.cs
@@ -2,17 +2,51 @@ namespace negri_do_plynu
 {
     public partial class Form1 : Form
     {
+        int cislo1 = 0;
+        int cislo2 = 0;
+        int vysledek = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
+        private void button0_Click(object sender, EventArgs e)
         {
 
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
 
-        private void label1_Click(object sender, EventArgs e)
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button9_Click(object sender, EventArgs e)
         {
 
         }
